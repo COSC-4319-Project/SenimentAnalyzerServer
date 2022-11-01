b11178f23a3addbc9d701a8f89b15135f333af1a
Tilly Dewing <dewing.tilly@gmail.com>
2022-11-01 12:30:32 -0500

    SQL connection

--- SQLConnection.cs
+++ SQLConnection.cs
@@ -3,15 +3,81 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace SenimentAnalyzerServer
 {
     class SQLConnection
     {
+        private static string server = "localhost";
+        private static string userid = "CMBD";
+        private static string password = "password";
+        private static string database = "Reviews";
+
+        private static MySqlConnection con;
+        private static MySqlCommand cmd;
+
+        private static MySqlDataReader rdr;
+
+        public static void InitializeDatabase()
+        {
+            cmd = new MySqlCommand();
+            cmd.Connection = con;
+
+            cmd.CommandText = @"CREATE TABLE IF NOT EXISTS login(sId int , sName varchar(40), sUser varchar(20), sPassword varchar(255), primary key(sUser, sID)";
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = @"CREATE TABLE IF NOT EXISTS reviews(sentVal int, numRev int, numPos int, numNeg int, adjustedRating float, confidence float, UId int, ASINID varchar(10), IDDate Date, foreign key(UId) references Login(sId), primary key(UId,ASINID))";
+            cmd.ExecuteNonQuery();
+        }
+
+        public static bool AttemptSQLConnection(string username, string password)
+        {
+            string cs = @"server=" + server + ";userid=" + username + "; password=" + password + ";database=" + database;
+
+            con = new MySqlConnection(cs);
+
+            try
+            {
+                con.Open();
+                InitializeDatabase();
+
+                return true;
+            }
+            catch (MySqlException)
+            {
+                Console.WriteLine("Error: Unable to connect to SQL database. Check login information and server status.");
+                return false;
+            }
+        }
+        public static void CloseConnection()
+        {
+            if (con != null)
+            {
+                con.Close();
+            }
+        }
+
+
         public static User GetUser(string username)
         {
             User user = new User();
-            //SQL DATABASE: Get User info from database 
+            cmd.CommandText = "SELECT * FROM login WHERE sUser=" + username;
+            rdr = cmd.ExecuteReader();
+
+            if (rdr.HasRows)
+            {
+                rdr.Read();
+                user.userID = rdr.GetInt32("sID");
+                user.username = rdr.GetString("sUser");
+
+                rdr.Close();
+            }
+            else
+            {
+                rdr.Close();
+            }
+
             return user;
         }
 
@@ -32,7 +98,7 @@ namespace SenimentAnalyzerServer
             //SQL DATABASE: Get History info from database for product id if found;
 
             //if not found
-            rec.numReviews = -1; //To indicate no record found
+            rec.numRev = -1; //To indicate no record found
             return rec;
         }
 
@@ -44,14 +110,20 @@ namespace SenimentAnalyzerServer
 
     struct User
     {
+        public int userID;
         public string username;
         public string password;
+        public string name;
     }
     struct HistoryRec
     {
-        public string productID;
-        public float modifedRating;
-        public int numReviews;
+        public string asinID;
+        public int sentimentVal;
+        public int numRev;
+        public int numNeg;
+        public int numPos;
+        public float confidence;
+        public float adjustedRating;
         public DateTime dateAnalyzed;
     }
 }
